b5a8c3e2cb90fd1110a330a4aff373065dc57676
emergent <yosikawa@gmail.com>
2019-02-10 23:55:45 +0900

    ルーティング設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { TodayComponent } from './today/today.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/today', pathMatch: 'full' },
+  { path: 'today', component: TodayComponent }
+];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }

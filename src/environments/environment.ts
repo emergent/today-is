6dfc5de02076360c9259c57b2f8e8e71f2ba4622
emergent <yosikawa@gmail.com>
2019-02-11 01:26:36 +0900

    lint対応

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDG5tKfB6bMLuRQRag2o921ouKtWt4BrN0",
-    authDomain: "today-is-b03f4.firebaseapp.com",
-    databaseURL: "https://today-is-b03f4.firebaseio.com",
-    projectId: "today-is-b03f4",
-    storageBucket: "today-is-b03f4.appspot.com",
-    messagingSenderId: "116203794119"
+    apiKey: 'AIzaSyDG5tKfB6bMLuRQRag2o921ouKtWt4BrN0',
+    authDomain: 'today-is-b03f4.firebaseapp.com',
+    databaseURL: 'https://today-is-b03f4.firebaseio.com',
+    projectId: 'today-is-b03f4',
+    storageBucket: 'today-is-b03f4.appspot.com',
+    messagingSenderId: '116203794119'
   }
 };
 

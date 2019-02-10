677639dc603772173543f75fd45eda2edf9d96bb
emergent <yosikawa@gmail.com>
2019-02-11 01:20:26 +0900

    deploy firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TodayComponent } from './today/today.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { TodayComponent } from './today/today.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDG5tKfB6bMLuRQRag2o921ouKtWt4BrN0",
+    authDomain: "today-is-b03f4.firebaseapp.com",
+    databaseURL: "https://today-is-b03f4.firebaseio.com",
+    projectId: "today-is-b03f4",
+    storageBucket: "today-is-b03f4.appspot.com",
+    messagingSenderId: "116203794119"
+  }
 };
